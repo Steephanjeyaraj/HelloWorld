5ea0bce512aed6b4d9fc45c5d3973ed760b631a2
Steephanjeyaraj <steephanjeyaraj@gmail.com>
2018-03-31 23:22:04 +0530

    Update InstallDependencies.ps1

--- InstallDependencies.ps1
+++ InstallDependencies.ps1
@@ -40,6 +40,6 @@ $JMeterPath=""
 downloadFile("")
 unzip("")
 setEnvironmentalVariable("")
-
+New-NetFirewallRule -DisplayName "Master File Sharing (SMB-In)" -Description "Allows inbound traffic from Client to Access Files" -Group "File and Printer Sharing" -Action Allow -Direction Inbound -InterfaceType Any -Profile Any -LocalAddress Any -LocalPort 445 -RemoteAddress Any -RemotePort Any -Protocol TCP  
 
 
